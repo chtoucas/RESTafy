9ccb41ddaa0556e5eee115a1849d0a9a977668c5
Chtoucas <chtoucas@narvalo.org>
2013-06-09 21:30:24 +0200

    Miscs

--- libexec/runtest.php
+++ libexec/runtest.php
@@ -41,9 +41,9 @@ class RunTestApp {
     $stdout    = IO\File::GetStandardOutput();
     $outWriter = new Tap\TapOutWriter($stdout, \TRUE);
     $errWriter = new Tap\TapErrWriter($stdout);
-
     $engine    = new Framework\TestEngine($outWriter, $errWriter);
-    $producer  = new Framework\TestProducer($engine);
+
+    $producer = new Framework\TestProducer($engine);
     $producer->register();
 
     $runner = new Runner\TestRunner($producer);
