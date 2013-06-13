02d4d897e57e136a7dea6b2128be2af114a71a98
Chtoucas <chtoucas@narvalo.org>
2013-06-13 17:32:39 +0200

    More or less completed the php launcher.

--- bin/php.rb
+++ bin/php.rb
@@ -5,41 +5,58 @@
 # http://stackoverflow.com/questions/3159945/running-command-line-commands-within-ruby-script
 
 require 'rbconfig'
+require 'fileutils'
+
+#---------------------------------------------------------------------------------------------------
 
 def os
-    # http://stackoverflow.com/questions/11784109/detecting-operating-systems-in-ruby
+    # Cf. http://stackoverflow.com/questions/11784109/detecting-operating-systems-in-ruby
     @os ||= (
         case RbConfig::CONFIG['host_os']
         when /cygwin/
             then 'cygwin'
         when /darwin|mac os|linux|solaris|bsd/
             then 'unix'
-        else raise 'Unsupported OS'
+        else raise 'Unsupported OS.'
         end
     )
 end
 
 def _(path)
     # Cf. http://www.cygwin.com/cygwin-ug-net/using-utils.html#cygpath
-    # FIXME: There is a problem in cygwin when the path contains a tilde: ~
-    os == 'cygwin' ? (`cygpath -wal -- "#{path}"`).strip! : path
+    # NB: There is a problem in cygwin whith paths containing a tilde (short-name DOS style).
+    # Nevertheless when the file already exists, the problem disappears.
+    os == 'cygwin' ? %x(cygpath -law -- "#{path}").strip! : path
 end
 
-# Project directories.
+#---------------------------------------------------------------------------------------------------
+
+if ARGV.empty? then
+    raise 'Missing input file.'
+end
+
+# Project directory.
 dir = File.join(File.expand_path(File.dirname(__FILE__)), '..')
+# Temp directory.
 tmpdir = File.join(dir, 'tmp')
+Dir.mkdir_p(tmpdir) unless File.exists?(tmpdir)
+# Log file.
+logfile = File.join(tmpdir, 'php.log')
+FileUtils.touch(logfile) unless File.exists?(logfile)
 
-Dir.mkdir(tmpdir) unless File.exists?(tmpdir)
-
+# PHP inline configuration.
+ini = {
+    'include_path' => _(File.join(dir, 'lib')),
+    'error_log'    => _(logfile)
+}.map { |k, v| '-d "%s=%s"' % [k, v] }
+# Other arguments passed to the PHP interpreter.
 opts = {
     # PHP ini file.
     '-c' => _(File.join(dir, 'etc', 'php.ini')),
-    # PHP include path.
-    '-d' => 'include_path=%s' % _(File.join(dir, 'lib')),
-    # PHP error log.
-    #'-d' => 'error_log=%s' % _(File.join(tmpdir, 'php.log')),
-}
-args = opts.map{ |k, v| '%s "%s"' % [k, v]}.push(ARGV).join(' ')
+}.map { |k, v| '%s "%s"' % [k, v] }
+
+args = opts.push(ini).push(ARGV).join(' ')
 
 exec("/usr/bin/env php #{args}")
 
+# EOF
