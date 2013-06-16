2f6d476a5078813daa9d9e0e92a0081039eefadf
Chtoucas <chtoucas@narvalo.org>
2013-06-16 22:47:48 +0200

    Improved code quality of ruby code.

--- RESTafy.rb
+++ RESTafy.rb
@@ -15,43 +15,40 @@ class RESTafy
 
     # WARNING: Completely replace the current process.
     def exec
-        if ARGV.empty? then
-            raise 'ARGV can not be empty.'
-        end
+        if ARGV.empty? then raise 'ARGV can not be empty.' end
 
-        Kernel.exec(build_cmd(ARGV, false, true).to_s)
+        Kernel.exec build_cmd(ARGV, false, true).to_s
     end
 
     # WARNING: Completely replace the current process.
     def test
-        if ARGV.empty? then
-            raise 'ARGV can not be empty.'
-          end
+        if ARGV.empty? then raise 'ARGV can not be empty.' end
 
-        Kernel.exec(test_cmd(ARGV).to_s)
+        Kernel.exec test_cmd(ARGV).to_s
     end
 
     def prove(dir, blib)
-        system(prove_cmd(dir, blib).to_s)
+        Kernel.system prove_cmd(dir, blib).to_s
     end
 
     def lint
         errs = []
         pattern = File.join @env.lib_dir, '**', '*.php'
         Dir.glob(pattern) do |file|
-            stdin, stdout, stderr = Open3.popen3(lint_cmd(file, false).to_s)
-            unless stdout.readlines[0] =~ /^No syntax errors detected in/ then
-                errs.push(file)
+            stdin, stdout, stderr = Open3.popen3 lint_cmd(file, false).to_s
+            unless stdout.readlines[0].chomp! =~ %r{^No syntax errors detected in} then
+                errs.push file
             end
             print '.'
         end
-        puts ''
-        if errs.length == 1 then
-            warn 'There is 1 malformed file.'
-        elsif errs.length > 1 then
-            warn 'There are %s malformed files.' % errs.length
-        else
+        puts %q{}
+        if errs.length == 0 then
             success 'No syntax errors detected.'
+        elsif errs.length == 1 then
+            warn %q{There is 1 malformed file: "%s"} % errs[0].sub(@env.lib_dir.chop, %q{})
+        else
+            warn 'There are %s malformed files:' % errs.length
+            errs.each { |e| warn %q{  lib%s} % e.sub(@env.lib_dir, %q{}) }
         end
     end
 
@@ -65,24 +62,24 @@ class RESTafy
     def build_cmd(argv, blib, debug)
         cmd = PHPCmd.new
         cmd.argv = argv
-        cmd.ini('include_path', blib ? blib_dir : lib_dir)
-        cmd.ini('error_log', log_file)
-        cmd.opt('-c', debug ? ini_dbg : ini)
+        cmd.ini 'include_path', blib ? blib_dir : lib_dir
+        cmd.ini 'error_log', log_file
+        cmd.opt '-c', debug ? ini_dbg : ini
         return cmd
     end
 
     def prove_cmd(dir, blib)
         exe = File.join @env.libexec_dir, 'prove.php'
-        build_cmd([quoted_path(exe), dir], blib, false)
+        build_cmd [quoted_path(exe), dir], blib, false
     end
 
     def test_cmd(argv)
         exe = File.join @env.libexec_dir, 'runtest.php'
-        build_cmd([quoted_path(exe)].push(argv), false, false)
+        build_cmd [quoted_path(exe)].push(argv), false, false
     end
 
     def lint_cmd(file, blib)
-        build_cmd(['-l', quoted_path(file)], blib, false)
+        build_cmd ['-l', quoted_path(file)], blib, false
     end
 
     private
@@ -94,18 +91,18 @@ class RESTafy
     def log_file; @log_file ||= path(@env.log_file) end
 
     def quoted_path(path)
-        '"' + path(path) + '"'
+        %q{"} + path(path) + %q{"}
     end
 
     def path(path)
         # Cf. http://www.cygwin.com/cygwin-ug-net/using-utils.html#cygpath
         # NB: There is a problem in cygwin whith paths containing a tilde (short-name DOS style).
         # Nevertheless when the file already exists, the problem disappears.
-        @env.is_cygwin? ? %x(cygpath -law -- "#{path}").strip! : path
+        @env.is_cygwin? ? %x{cygpath -law -- "#{path}"}.chomp! : path
     end
 
-    def warn(text);    puts "\033[31m#{text}\033[0m" end
-    def success(text); puts "\033[32m#{text}\033[0m" end
+    def warn(text);    puts %Q{\033[31m#{text}\033[0m} end
+    def success(text); puts %Q{\033[32m#{text}\033[0m} end
 end
 
 
@@ -120,9 +117,9 @@ class RESTafyEnv
 
     def self.prepare
         inst = self.instance
-        Dir.mkdir(inst.tmp_dir) unless File.exists?(inst.tmp_dir)
+        Dir.mkdir inst.tmp_dir unless File.exists? inst.tmp_dir
         if inst.is_cygwin? then
-            FileUtils.touch(inst.log_file) unless File.exists?(inst.log_file)
+            FileUtils.touch inst.log_file unless File.exists? inst.log_file
         end
     end
 
@@ -140,9 +137,9 @@ class RESTafyEnv
     def is_cygwin?
         @is_cygwin ||= (
             case RbConfig::CONFIG['host_os']
-            when /cygwin/
+            when %r{cygwin}
                 then true
-            when /darwin|mac os|linux|solaris|bsd/
+            when %r{darwin|mac os|linux|solaris|bsd}
                 then false
             else raise 'Unsupported OS.'
             end
@@ -178,13 +175,13 @@ class PHPCmd
 
     def to_s
         # INI settings.
-        ini  = @ini.map { |k, v| '-d "%s=%s"' % [k, v] }
+        ini  = @ini.map { |k, v| %q{-d "%s=%s"} % [k, v] }
         # Other arguments passed to the PHP interpreter.
-        opts = @opts.map { |k, v| '%s "%s"' % [k, v] }
+        opts = @opts.map { |k, v| %q{%s "%s"} % [k, v] }
 
         args = opts.push(ini).push(@argv).join(' ')
 
-        "#{@@exe} #{args}"
+        %Q{#{@@exe} #{args}}
     end
 end
 
